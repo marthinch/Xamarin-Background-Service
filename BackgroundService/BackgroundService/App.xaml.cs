9961ac5a9e3db0f537c1fb6eccd7e26fafb72a58
Marthin Silalahi <marthin.silalahi@anacle.com>
2020-08-17 14:16:27 +0800

    INITIALIZE PROJECT & GIT IGNORE

---
+++ BackgroundService/BackgroundService/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using BackgroundService.Services;
+using Matcha.BackgroundService;
+using Xamarin.Forms;
+
+namespace BackgroundService
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            StartBackgroundService();
+        }
+
+        protected override void OnSleep()
+        {
+           
+        }
+
+        protected override void OnResume()
+        {
+            
+        }
+
+        private static void StartBackgroundService()
+        {
+            BackgroundAggregatorService.Add(() => new PeriodicTask(10));
+
+            // Running the periodic task in the background
+            BackgroundAggregatorService.StartBackgroundService();
+        }
+    }
+}
---
+++ BackgroundService/BackgroundService/Services/PeriodicTask.cs
@@ -0,0 +1,24 @@
+﻿using Matcha.BackgroundService;
+using Plugin.LocalNotifications;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BackgroundService.Services
+{
+    public class PeriodicTask : IPeriodicTask
+    {
+        public PeriodicTask(int interval)
+        {
+            Interval = TimeSpan.FromSeconds(interval);
+        }
+
+        public TimeSpan Interval { get; set; }
+
+        public Task<bool> StartJob()
+        {
+            CrossLocalNotifications.Current.Show("Test local notification", "This notification is running from background service");
+            return Task.FromResult(true);
+        }
+    }
+}
